c5c1154eb9a1dfd8a371a19186b7fb7756d3c08c
Dan Michael O. Heggø <danmichaelo@gmail.com>
2013-08-18 01:11:56 +0200

    Added laravel config file

---
+++ config/config.php
@@ -0,0 +1,29 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| NCIP endpoint URL
+	|--------------------------------------------------------------------------
+	|
+	| URL to the NCIP (NISO Circulation Interchange Protocol, also known as 
+	| Z39.83) endpoint.
+	|
+	*/
+
+	'url' => 'localhost',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Agency ID
+	|--------------------------------------------------------------------------
+	|
+	| The id of the agency; an organization that lends items or provides other 
+	| services to users. 
+	|
+	*/
+
+	'agency_id' => ''
+
+);
